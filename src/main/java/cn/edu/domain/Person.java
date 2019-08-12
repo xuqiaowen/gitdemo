767c7fa6d08b3acade9fa0fe6648fa4156812838
xqw <24775321@qq.om>
2019-08-12 13:01:54 +0800

    112

---
+++ src/main/java/cn/edu/domain/Person.java
@@ -0,0 +1,31 @@
+package cn.edu.domain;
+
+public class Person {
+    private String name;
+    private String sex;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
